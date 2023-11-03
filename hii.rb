66c2c30a79bca45b50a47dff5f234c8e2ab87db9
bapun nath <rinkusahoo181@gmail.com>
2023-11-04 02:31:29 +0530

    first commit

---
+++ hii.rb
@@ -0,0 +1,10 @@
+num1= 23
+num2=34
+num3 =45
+if num1 >= num2 && num1 >= num3
+    puts "#{num1} is greter numver"
+
+elseif  num2 >= num1 &&num2 >= num3
+    puts "#{num2} is greter numver"
+else puts "#{num3} is greter numver"
+end
